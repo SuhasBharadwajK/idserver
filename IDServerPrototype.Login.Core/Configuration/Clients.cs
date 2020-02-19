c25650f2d90e69b2b10914d46c6df22e7f6186f0
Suhas Bharadwaj <suhas@keka.com>
2020-02-19 15:20:36 +0530

    Add project files.

---
+++ IDServerPrototype.Login.Core/Configuration/Clients.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using System.Collections;
+using System.Collections.Generic;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace IDServerPrototype.Login.Core.Configuration
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientName = "ResourceOwnerClient",
+                    ClientId = "roclient",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("roclientsecret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes = new List<string>
+                    {
+                        StandardScopes.OpenId,
+                        StandardScopes.Profile,
+                        "api"
+                    },
+                },
+                new Client
+                {
+                    ClientId = "oauthClient",
+                    ClientName = "Example Client Credentials Client Application",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("superSecretPassword".Sha256())
+                    },
+                    AllowedScopes = new List<string> { "customAPI.read", "https://localhost:44350/resources" },
+                }
+            };
+        }
+    }
+}
---
+++ IDServerPrototype.Login.Core/Configuration/Scopes.cs
@@ -0,0 +1,35 @@
+﻿using IdentityServer4.Models;
+using System.Collections;
+using System.Collections.Generic;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace IDServerPrototype.Login.Core.Configuration
+{
+    internal class Scopes
+    {
+        public static IEnumerable Get()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = StandardScopes.OpenId,
+                    DisplayName = "OpenId",
+                    Description = "OpenId scope",
+                },
+                new Scope
+                {
+                    Name = StandardScopes.Profile,
+                    DisplayName = "Profile",
+                    Description = "Profile scope",
+                },
+                new Scope
+                {
+                    Name = "api",
+                    DisplayName = "Bank API",
+                    Description = "Bank API scope",
+                }
+            };
+        }
+    }
+}
